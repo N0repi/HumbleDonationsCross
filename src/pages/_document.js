b2dd3aab09a403f576bb61164dd897998235c815
N0repi <cavantsmith98@gmail.com>
2024-12-07 02:44:06 -0800

    More SEO

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,6 +7,14 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <meta charset="UTF-8" />
+          <meta
+            name="description"
+            content="Humble Donations is a decentralized crowdfunding and donation platform where you can support creators, startups, and causes with zero fees utilizing crypto using blockchain technology."
+          />
+          <meta
+            name="keywords"
+            content="crypto donations, cryptocurrency, crowdfunding, HumbleDonations, Humble Donations, HDT, support creators, blockchain donations, earn rewards, staking HDT-WETH, web3 fundraising, no revenue cuts, decentralized, Arbitrum, NGO, startups, artists"
+          />
           <meta name="robots" content="index, follow" />
           <meta name="author" content="HumbleDonations Team" />
           <link rel="icon" href="/favicon.ico" type="image/x-icon" />
